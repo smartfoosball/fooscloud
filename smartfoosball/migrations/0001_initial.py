f0ab121c5dc0e69084a8233cba7cd84513ae82d2
Robert <rzheng@xtremeprog.com>
2014-12-06 18:27:10 +0800

    models

--- smartfoosball/migrations/0001_initial.py
+++ smartfoosball/migrations/0001_initial.py
@@ -13,13 +13,42 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Game',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('red_score', models.IntegerField(default=0)),
+                ('blue_score', models.IntegerField(default=0)),
+                ('status', models.IntegerField(default=1, choices=[(0, b'unknown'), (1, b'waiting'), (2, b'playing'), (3, b'end')])),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Goal',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('position', models.IntegerField(choices=[(0, b'unknown'), (1, b'red_van'), (2, b'red_rear'), (3, b'blue_van'), (4, b'blue_rear')])),
+                ('team', models.IntegerField(choices=[(0, b'unknown'), (1, b'red'), (2, b'blue')])),
+                ('game', models.ForeignKey(to='smartfoosball.Game')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Player',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
                 ('openid', models.CharField(max_length=32)),
-                ('access_token', models.CharField(max_length=32)),
+                ('access_token', models.CharField(max_length=256)),
                 ('expires_at', models.IntegerField()),
-                ('refresh_token', models.CharField(max_length=32)),
+                ('refresh_token', models.CharField(max_length=256)),
                 ('scope', models.CharField(max_length=256)),
                 ('nickname', models.CharField(max_length=128)),
                 ('headimgurl', models.URLField()),
@@ -28,5 +57,35 @@ class Migration(migrations.Migration):
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='goal',
+            name='player',
+            field=models.ForeignKey(to='smartfoosball.Player'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='blue_rear',
+            field=models.ForeignKey(related_name='blue_rear_games', blank=True, to='smartfoosball.Player', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='blue_van',
+            field=models.ForeignKey(related_name='blue_van_games', blank=True, to='smartfoosball.Player', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='red_rear',
+            field=models.ForeignKey(related_name='red_rear_games', blank=True, to='smartfoosball.Player', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='red_van',
+            field=models.ForeignKey(related_name='red_van_games', blank=True, to='smartfoosball.Player', null=True),
+            preserve_default=True,
         ),
     ]
--- smartfoosball/models.py
+++ smartfoosball/models.py
@@ -1,8 +1,21 @@
+from enum import Enum
 from django.db import models
 from django.contrib.auth.models import User
 
 
+class Enumm(Enum):
+
+    @classmethod
+    def choices(cls):
+        choices = []
+        for i in cls:
+            choices.append((i.value, i.name))
+        return tuple(choices)
+
+
 class Player(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     openid = models.CharField(max_length=32)
     access_token = models.CharField(max_length=256)
     expires_at = models.IntegerField()
@@ -11,6 +24,47 @@ class Player(models.Model):
     nickname = models.CharField(max_length=128)
     headimgurl = models.URLField()
     user = models.OneToOneField(User)
+
+
+class Game(models.Model):
+    
+    class Status(Enumm):
+        unknown = 0
+        waiting = 1
+        playing = 2
+        end = 3
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    red_van  = models.ForeignKey(Player, related_name='red_van_games', blank=True, null=True)
+    red_rear = models.ForeignKey(Player, related_name='red_rear_games', blank=True, null=True)
+    blue_van = models.ForeignKey(Player, related_name='blue_van_games', blank=True, null=True)
+    blue_rear = models.ForeignKey(Player, related_name='blue_rear_games', blank=True, null=True)
+    red_score = models.IntegerField(default=0)
+    blue_score = models.IntegerField(default=0)
+    status = models.IntegerField(choices=Status.choices(), default=Status.waiting.value)
+
+
+class Goal(models.Model):
+
+    class Position(Enumm):
+        unknown = 0
+        red_van = 1
+        red_rear = 2
+        blue_van = 3
+        blue_rear = 4
+
+    class Team(Enumm):
+        unknown = 0
+        red = 1
+        blue = 2
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    game = models.ForeignKey(Game)
+    player = models.ForeignKey(Player)
+    position = models.IntegerField(choices=Position.choices())
+    team = models.IntegerField(choices=Team.choices())
+    
 
 # class Games(models.Model):
 # #    sn = models.IntegerField(primary_key=True)
