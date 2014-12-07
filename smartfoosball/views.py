dbe2d71b1b94e5a38e0cbdf3d35a0823dbdec126
Robert <rzheng@xtremeprog.com>
2014-12-07 17:05:52 +0800

    page me

--- smartfoosball/views.py
+++ smartfoosball/views.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 from django.views.generic import View,TemplateView
+from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
@@ -28,83 +29,6 @@ class Index(TemplateView):
     template_name = 'index.html'
     def get(self, request):
         return super(Index, self).get(request)
-
-
-# class PlayerRegister(View):
-#     def get(self, request, nick_name):
-#         _nick_name = str(nick_name)
-#         _player_auth_sn=str(uuid.uuid4())
-#         p = Players(wechat_id='', auth_sn=_player_auth_sn, nick_name=_nick_name)
-#         p.save()
-#         return HttpResponse(_player_auth_sn)
-
-
-# class GameCreate(View):
-#     def get(self, request, auth_sn):
-#         # create game
-#         _game_id = str(uuid.uuid4())
-#         print _game_id
-#         game_model = Games(game_id=_game_id,
-#                            game_status=GameStatusEn.waiting.value,
-#                            creater_sn=get_player_by_auth(str(auth_sn)),
-#                            created_at=datetime.now()
-#                            )
-#         game_model.save(force_insert=True)
-#         #create position
-#         generate_position_blank(game_model)
-
-#         return HttpResponse(_game_id)
-
-
-# class GamePosition(View):
-#     def get(self, request, game_id, auth_sn, team_id, position_id):
-#         _game_id = str(game_id)
-#         _game_sn = gameid_to_gamesn(_game_id)
-#         _auth_sn = str(auth_sn)
-#         _team_id = int(team_id)
-#         _position_id = int(position_id)
-#         print _game_sn
-#         pos = Positions.objects.filter(game_sn=_game_sn,
-#                                        team_id=_team_id,
-#                                        pos_id=_position_id,
-#                                        ).first()
-#         if pos:
-#             if pos.player_sn.sn == god_player_sn():
-#                 pos.player_sn = get_player_by_auth(_auth_sn)
-#                 pos.save()
-#                 return HttpResponse('OK')
-#             else:
-#                 return HttpResponse('player_exists')
-#         else:
-#             return HttpResponse('Position Not found')
-
-
-# class GameScore(TemplateView):
-#     template_name = 'score_board.html'
-
-#     def get(self, request, game_id):
-#         '''
-#         struct:
-#         {
-#         'game_id':str,
-#         'teams':{1:{1:player_object, 2:player_object}, 2:{1:player_obj,2:player_obj}},
-        
-#         }
-#         '''
-# #        return render_to_response(self.template_name)
-#         gsn = get_game_by_game_id(game_id)
-#         score_struct = {'game_id': game_id}
-#         teams = {TeamEn.red.name: None, TeamEn.blue.name: None}
-#         if gsn:
-#             pos = Positions.objects.filter(game_sn=gsn).all()
-#             # red
-#             teams[TeamEn.red.name] = {PositionEn.attack.name:get_player(pos, TeamEn.red.value, PositionEn.attack.value)}
-#             teams[TeamEn.red.name].update({PositionEn.defence.name:get_player(pos, TeamEn.red.value, PositionEn.defence.value)})
-#             # blue
-#             teams[TeamEn.blue.name] = {PositionEn.attack.name:get_player(pos, TeamEn.blue.value, PositionEn.attack.value)}
-#             teams[TeamEn.blue.name].update({PositionEn.defence.name:get_player(pos, TeamEn.blue.value, PositionEn.defence.value)})
-#         score_struct = {'teams':teams}
-#         return render_to_response('score_board.html', {"score_board":score_struct})
         
 
 class WechatEcho(View):
@@ -239,7 +163,20 @@ class PlayerView(View):
 class MeView(BaseWeixinView):
 
     def get(self, request):
-        return HttpResponse("me")
+        me = request.user.player
+        win, lost = me.performance()
+        performances = []
+        for i in Player.objects.all():
+            performances.append((i, i.performance()[0]))
+        sorted(performances, key=lambda x: x[1], reverse=True)
+        ctx = RequestContext(
+            request, 
+            {"win": win, 
+             "lost": lost,
+             "tot_players": len(performances),
+             "rank": performances.index((me, win)) + 1,
+             "partners": me.partners()})
+        return render_to_response('me.html', ctx)
 
 
 def wechat_oauth2(request):
